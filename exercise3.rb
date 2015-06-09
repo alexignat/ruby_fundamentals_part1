3015bbd7d423cf21f80034456bedfda9379082b6
alexignat <d.alexandru.ignat@gmail.com>
2015-06-09 16:12:30 -0400

    Finished age program

--- exercise3.rb
+++ exercise3.rb
@@ -4,4 +4,14 @@ puts "What is your name?"
 
 name = gets.chomp.capitalize
 
-puts "Hi #{name}"
+puts "Hi #{name}!"
+
+# age program
+
+puts "How old are you?"
+
+age = gets.chomp.to_i
+current_year = 2015
+birth_year = current_year - age
+
+puts "You were born in #{birth_year}."
