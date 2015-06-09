4f9e1b92b1f47a4292d6e3f7b06d0dbe79df1e49
alexignat <d.alexandru.ignat@gmail.com>
2015-06-09 16:33:28 -0400

    Fully functional

--- exercise4.rb
+++ exercise4.rb
@@ -3,19 +3,17 @@
 # For multiples of 3 & 5 print "BitMaker"
 
 
-# This version does not work correctly
-# For a number divisible by 3 & 5, it will print "bit" and then "bitmaker"
+#Fully functional 
+
 for i in (1..100)
-	if i % 3 == 0 
+	if (i % 3 == 0) && (i % 5 == 0)
+		puts "BitMaker"
+	elsif i % 3 == 0 
 		puts "Bit"
 	elsif i % 5 == 0
 		puts "Maker"
 	else puts i
-	end
-
-	if (i % 3 == 0) && (i % 5 == 0)
-		puts "BitMaker"
-	end
+	end	
 end
 
 
