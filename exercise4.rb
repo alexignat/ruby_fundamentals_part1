ef394ed1f637f098482a7f024de54d875f3430c9
alexignat <d.alexandru.ignat@gmail.com>
2015-06-09 16:26:23 -0400

    Edited comments to explain problem

--- exercise4.rb
+++ exercise4.rb
@@ -2,6 +2,9 @@
 # For multiples of 5 print "Maker"
 # For multiples of 3 & 5 print "BitMaker"
 
+
+# This version does not work correctly
+# For a number divisible by 3 & 5, it will print "bit" and then "bitmaker"
 for i in (1..100)
 	if i % 3 == 0 
 		puts "Bit"
@@ -14,3 +17,23 @@ for i in (1..100)
 		puts "BitMaker"
 	end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
