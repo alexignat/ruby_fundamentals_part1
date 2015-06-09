092d97daca5bcfc2596a7ca2926b0826300557e4
alexignat <d.alexandru.ignat@gmail.com>
2015-06-09 15:55:51 -0400

    Playing with strings

--- exercise1.rb
+++ exercise1.rb
@@ -4,7 +4,14 @@ puts 2
 puts 3
 puts 2 != 3
 
-#single quotes 
+# single quotes 
 
 puts "Betty's pie shop"
 puts 'Betty\'s pie shop'
+
+# playing with strings 
+
+puts "Hello\t\tworld" # \t tabs
+puts "Hello\b\b\b\b\bGoodbye world" #\b backspaces
+puts "Hello\rStart over world" #\ carriage return
+puts "1. Hello\n2. World" #\ new line
