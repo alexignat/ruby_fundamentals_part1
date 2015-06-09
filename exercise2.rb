77f35379e785099866171871a509a1d963c2335e
alexignat <d.alexandru.ignat@gmail.com>
2015-06-09 16:06:11 -0400

    Finished problems 1-4

---
+++ exercise2.rb
@@ -0,0 +1,23 @@
+# Problem 1: Calculating a tip
+
+meal = 55
+
+tip = meal * 0.15
+
+puts "The appropriate tip for a meal that costs $55 is #{tip}"
+
+# Problem 2: Adding a string and interger
+
+puts "2" + 2.to_s
+
+# Problem 3: Multiplying and printing a big number
+
+big_number = 45628 * 7839
+
+puts "#{big_number} is a big number!"
+
+# Problem 4: True or false!?
+
+tricky = (true && false) || (false && true) || !(false && false)
+
+puts "This mumbo jumbo is #{tricky}. I've entered the matrix, I now see the truth."
